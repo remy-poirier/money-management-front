dc438de2d4a7c9ad38a490e8b26db4df0602f7d5
Remy-Poirier <remy.poirier92@gmail.com>
2024-02-14 14:02:45 +0100

    Minor updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Button} from "@/components/ui/button.tsx";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
+import {ModeToggle} from "@/components/mode-toggle.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>
+            <Button>Click me</Button>
+            <ModeToggle />
+        </div>
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <RouterProvider router={router} />
+      </ThemeProvider>
   </React.StrictMode>,
 )
