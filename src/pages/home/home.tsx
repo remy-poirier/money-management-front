3ebc112b1c5a4e4679772b1de769a5cc068575d5
Rémy Poirier <remy.poirier@particeep.com>
2024-03-02 15:06:42 +0100

    Add sparkles component on home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,20 +5,48 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card.tsx'
+import { SparklesCore } from '@/components/sparkles.tsx'
+import { useTheme } from '@/components/theme-provider.tsx'
+import { Separator } from '@/components/ui/separator.tsx'
 
 export const Home = () => {
+  const { theme } = useTheme()
+
   return (
     <>
-      <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
-        <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">
-          Money Manager
+      <div className="h-[40rem] w-full bg-background flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <h1 className="md:text-6xl text-4xl lg:text-8xl font-bold text-center text-foreground relative z-20">
+          Money-Manager
         </h1>
-        <span className="max-w-[750px] text-center text-lg text-muted-foreground sm:text-xl">
+        <div className="w-[40rem] h-40 relative">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+
+          {/* Core component */}
+          <SparklesCore
+            background="transparent"
+            minSize={0.1}
+            maxSize={1.3}
+            particleDensity={1500}
+            className="w-full h-full"
+            particleColor={theme === 'dark' ? '#fff' : '#000'}
+          />
+
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-background [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+        </div>
+      </div>
+      <Separator />
+      <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-4 md:pb-4 lg:py-12 lg:pb-12">
+        <span className="max-w-[750px] text-center text-xl lg:text-3xl text-muted-foreground sm:text-xl">
           Prenez le contrôle de vos finances dès aujourd&apos;hui avec cette
           application de gestion des dépenses intelligente!
         </span>
       </section>
-      <section className="max-w-full px-0 md:px-10">
+      <section className="max-w-full px-0 md:px-10 pb-10">
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-3 w-full">
           <Card className="w-full">
             <CardHeader>
